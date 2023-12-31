a7a754b33bd389458391dd9cd106fc5cf2ae03fe
Van Thiep Nguyen <van-thiep.nguyen@ekwateur.fr>
2023-12-31 17:51:05 +0100

    finished chapter 16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Corp',
+    default: 'Acme Corp'
+  },
+  description: 'Acme Corp',
+  metadataBase: new URL('https://nextjs-dashboard-jetx.vercel.app'),
+};
 
 export default function RootLayout({
   children,
