1194636d29e641a53ff8d4fabb8724b1e0b145c6
Van Thiep Nguyen <van-thiep.nguyen@ekwateur.fr>
2023-12-31 10:04:09 +0100

    finished chapter 12

--- next.config.js
+++ next.config.js
@@ -1,4 +1,44 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return process.env.NODE_ENV !== 'production' ? [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:8200/:path*',
+            },
+            {
+                source: '/media/:path*',
+                destination: 'http://localhost:9000/:path*',
+            },
+        ] : []
+    },
+    images: {
+        remotePatterns: process.env.NODE_ENV !== 'production' ? [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '',
+                pathname: '/media/**',
+            },
+        ] : [
+            {
+                protocol: 'https',
+                hostname: 'qa.myappdomain.au',
+                port: '',
+                pathname: '/media/**',
+            },
+        ],
+    },
+    experimental: {
+        serverActions: {
+            allowedOrigins: [
+                'http://localhost:8200',
+                'http://localhost:8201',
+                'http://localhost:8202',
+                'http://localhost:3000',
+            ]
+        }
+    }
+};
 
 module.exports = nextConfig;
